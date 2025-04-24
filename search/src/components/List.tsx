010ef16472f854264d4b8cb388e1a2600291fa55
Yin Chantha <yin.chantha19@kit.edu.kh>
2025-04-24 21:02:49 +0700

    search redux setup

--- search/src/components/List.tsx
+++ search/src/components/List.tsx
@@ -1,18 +1,51 @@
-import Search from "./Search"
+import { useDispatch, useSelector } from "react-redux";
+import Search from "./Search";
+import { useEffect } from "react";
+import { fetchListAction } from "../redux/action/listAction";
+import { ThunkDispatch } from "redux-thunk";
 
 const List = () => {
+  const dispatch: ThunkDispatch<any, any, any> = useDispatch();
+  useEffect(() => {
+    dispatch(fetchListAction());
+  }, []);
+
+  const { loading, error, lists, post } = useSelector((data: any) => data);
+  console.log(loading, error, lists, post);
+
   return (
     <>
-        <Search />
-        <br />
-        <div className="mt-3">
-            <a href="#" className="block bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-3">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </a>
-        </div>
+      <Search />
+      <br />
+      <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+        Total Posts {lists.length}
+      </h1>
+      {loading ? (
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 mt-16">Loading...</h2>
+      ) : error ? (
+        <h2 className="text-red-500">
+          {error.response.status && "No Post Found"}
+        </h2>
+      ) : (
+        lists.map((d: any) => (
+          <div className="mt-3" key={d.id}>
+            {/* name, email and website */}
+            <div className="block bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 p-3">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {d.name}
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                {d.email},{" "}
+                <a href={d.website} target="_blank" className="text-blue-600 italic">
+                  {d.website}
+                </a>
+              </p>
+            </div>
+          </div>
+        ))
+      )}
     </>
-  )
-}
+  );
+};
 
-export default List     
+export default List;
---
+++ search/src/redux/action/listAction.tsx
@@ -0,0 +1,74 @@
+import axios from "axios"
+import { FETCH_ERRORE, FETCH_REQUEST, FETCH_SUCCESS, SEARCH_ERRORE, SEARCH_REQUEST, SEARCH_SUCCESS } from "../../constraints/actionType"
+import apiURL from "../../utils/apiUrl"
+
+
+// action fetch type
+const fetchListRequest = () => {
+    return {
+        type: FETCH_REQUEST
+    }
+}
+
+const fetchListSuccess = (list:any) => {
+    return {
+        type: FETCH_SUCCESS,
+        payload: list,
+    }
+}
+
+const fetchListErrore = (error: any) => {
+    return {
+        type: FETCH_ERRORE,
+        payload: error,
+    }
+}
+
+// action fetch 
+export const fetchListAction = () => {
+    return async (dispatch: any) => {
+        dispatch(fetchListRequest());
+        try {
+            const res = await axios.get(apiURL)
+            dispatch(fetchListSuccess(res.data))
+        } catch (error) {
+            dispatch(fetchListErrore(error))
+        }
+    }
+}
+
+// Search list actio type
+const searchListRequest = () => {
+    return {
+        type: SEARCH_REQUEST
+    }
+}
+
+const searchListSuccess = (list:any) => {
+    return {
+        type: SEARCH_SUCCESS,
+        payload: list,
+    }
+}
+
+const searchListErrore = (error: any) => {
+    return {
+        type: SEARCH_ERRORE,
+        payload: error,
+    }
+}
+
+
+// fetch sing list action
+
+export const fetchSingleListAction = (search: any) => {
+    return async (dispatch: any) => {
+        dispatch(searchListRequest());
+        try {
+            const res = await axios.get(`${apiURL}?${search}`)
+            dispatch(searchListSuccess(res))
+        } catch (error) {
+            dispatch(searchListErrore(error))
+        }
+    }
+}
